027cb2ad1071481f22b21e2ea0392f80c1da761b
vojtatom <tomas@vojtatom.cz>
2020-09-26 16:25:37 +0200

    Adding base and tutorials

---
+++ img.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    let imgs = document.getElementsByTagName("img");
+
+    for(let i of imgs) {
+        i.onclick = () => {
+            let s = document.getElementsByTagName("body")[0];
+            
+            let frame = document.createElement('div');
+            frame.classList.add('img-frame');
+
+            frame.onclick = () => {
+                frame.parentElement.removeChild(frame);
+            }
+
+            let image = document.createElement('img');
+            image.src = i.src;
+
+            frame.appendChild(image);
+            s.appendChild(frame);
+        }
+    }
+
+};
